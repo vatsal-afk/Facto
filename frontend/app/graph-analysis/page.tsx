ae3a58b4cee299ee0c21ecd98ee1fd84667e9909
Tanmay Sharma <148971810+cypher4802@users.noreply.github.com>
2025-01-03 04:47:05 +0530

    Update page.tsx

--- frontend/app/graph-analysis/page.tsx
+++ frontend/app/graph-analysis/page.tsx
@@ -1,11 +1,11 @@
-import NewsHeader from '@/components/news-analysis/news-header'
+// import NewsHeader from '@/components/news-analysis/news-header'
 import { NewsAnalysisReport } from "@/components/news-analysis/news-analysis-report"
 
 
 export default function NewsAnalysisPage({ params }: { params: { id: string } }) {
   return (
     <div className="container mx-auto px-4 py-8">
-      <NewsHeader id={params.id} />
+{/*       <NewsHeader id={params.id} /> */}
       <div className="container mx-auto px-4 py-8">
         <NewsAnalysisReport />
       </div>
