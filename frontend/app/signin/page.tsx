eb561d6d88912e188c5e10580c54301da953b6a1
Tanmay Sharma <148971810+cypher4802@users.noreply.github.com>
2025-01-03 04:47:36 +0530

    refined auth

--- frontend/app/signin/page.tsx
+++ frontend/app/signin/page.tsx
@@ -1,12 +1,14 @@
 import LoginForm from "@/components/LoginForm";
-// import { getServerSession } from "next-auth";
-// import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+
 
 
 export default async function Home() {
-//   const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-//   if (session) redirect("/dashboard");
+  if (session) redirect("/");
 
   return (
     <main>
